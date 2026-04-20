146a73e73fc2fa793642dcf1ac2cc2ff2a568670
daminiaror <136906311+daminiaror@users.noreply.github.com>
2026-04-21 01:59:46 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,17 +8,18 @@ async function bootstrap(): Promise<void> {
 
   app.useGlobalFilters(new PrismaClientKnownExceptionFilter());
 
-  app.enableCors({
-    origin: [
-      'http://localhost:3001',
-      'http://127.0.0.1:3001',
-      'http://localhost:3002',
-      'http://127.0.0.1:3002',
-      'http://localhost:3000',
-      'http://127.0.0.1:3000',
-    ],
-    credentials: true,
-  });
+app.enableCors({
+  origin: [
+    'http://localhost:3001',
+    'http://127.0.0.1:3001',
+    'http://localhost:3002',
+    'http://127.0.0.1:3002',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://locumlink-frontend-nd48.vercel.app',  
+  ],
+  credentials: true,
+});
   app.setGlobalPrefix('api');
 
   const config = app.get(ConfigService);
